ac464df6f7fd1f9b77a93ff4c7f88bbe2822bf74
Martín Peveri <martinpeveri@gmail.com>
2025-11-06 21:15:29 +0100

    Add comment to config files

--- config/initializers/api_version.rb
+++ config/initializers/api_version.rb
@@ -1,6 +1,8 @@
 # Set current stable version
 Rails.application.config.x.api_current_version = "2025-11-01"
+# Version files path
 Rails.application.config.api_version_base_path = "app/controllers/api/v1/versions"
+# Define version files for each API version
 Rails.application.config.x.version_files = {
   "2025-01-01" => [ "Api::V1::Versions::Version202501010001CombineFirstAndLastNameToNameInUser" ],
   "2025-11-01" => []
